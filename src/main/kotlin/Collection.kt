97e4f8b4ae14e0bcac3c7227d0324696c5cb3abf
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-11-02 15:43:42 +0100

    Collection stuff

---
+++ src/main/kotlin/Collection.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+//    val numbers = intArrayOf(1 , 4, 42, -3)
+//
+//    if (4 in numbers) {
+//        println("4 is in the array")
+//    }
+
+    val names = arrayOf("John", "Jane", "Mary", "Joe")
+
+    if ("Mary" in names) {
+        println("Mary is in the array")
+    }
+}
