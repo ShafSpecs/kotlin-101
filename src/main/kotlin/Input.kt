18a8f144e549b29390000b94fa0e678e551e9f0c
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-11-02 15:43:00 +0100

    Basic input

---
+++ src/main/kotlin/Input.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    print("Enter name: ")
+    val nameInput = readLine()!!
+
+    print("Enter message: ")
+    val messageInput = readLine()!!
+
+    println("$nameInput entered: $messageInput")
+}
