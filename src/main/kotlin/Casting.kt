a09219a2bd4aea2832a2cd59c4a9350741f813f3
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-11-03 11:28:52 +0100

    chore:Formatted file

--- src/main/kotlin/Casting.kt
+++ src/main/kotlin/Casting.kt
@@ -2,6 +2,6 @@ fun main(args: Array<String>) {
     val number1: Int = 545344
     val number2: Byte = number1.toByte()
 
-println("number1: $number1")
-println("number2: $number2")
+    println("number1: $number1")
+    println("number2: $number2")
 }
